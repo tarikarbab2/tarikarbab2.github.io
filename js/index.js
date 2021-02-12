eabc1fb8a9f76639eb302b7ebdb313b15598075e
tarikarbab2 <tarikarbab4@gmail.com>
2021-02-13 00:20:27 +0300

    secoend edition

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const nav=document.querySelector('.nav__icon')
+const items=document.querySelector('.nav__items')
+
+nav.addEventListener('click',function(e){
+    
+    items.style.display=items.style.display===''?'flex':"";
+
+})
+var myNav = document.querySelector('.nav');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ){
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
